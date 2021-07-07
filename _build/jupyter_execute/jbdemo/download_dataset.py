45a37ee4853a5911aaafb8ba9687688e584b14c1
Karanraj Chauhan <kachau@redhat.com>
2021-07-07 12:17:05 -0400

    test jupyterbook

---
+++ _build/jupyter_execute/jbdemo/download_dataset.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Download Dataset
+
+# ## Import libraries
+
+# In[2]:
+
+
+import os
+import logging
+import pickle
+
+
+# In[1]:
+
+
+print('dataset is downloaded')
+
---
+++ jbdemo/_build/jupyter_execute/download_dataset.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Download Dataset
+
+# ## Import libraries
+
+# In[1]:
+
+
+import os
+import logging
+import pickle
+
+
+# In[2]:
+
+
+print('dataset is downloaded')
+
